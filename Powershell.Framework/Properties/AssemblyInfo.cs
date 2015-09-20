6e7aaaea0f1dbb57a7c6736468cf8436c2ff3a02
tintoy <tintoy@tintoy.io>
2015-09-20 13:13:21 +1000

    Create initial skeleton.

---
+++ Powershell.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DDCloud.Powershell.Framework")]
+[assembly: AssemblyDescription("Powershell module framework")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("65d0797d-5f61-4a5d-a8ad-1733d4cc5bda")]
